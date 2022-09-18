bc38455adb0caab91306dd4cbae63223b19e62bd
Alisson <alisson42@protonmail.com>
2022-09-17 22:38:54 -0300

    Add mobile

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-import './scss/style.scss'
+import "./scss/style.scss";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const menus = document.querySelectorAll("[data-menu]");
+  menus.forEach((menu) => {
+    const targetId = menu.dataset.menu;
+    const target = document.getElementById(targetId);
+    menu.addEventListener("click", () => target.classList.toggle("open"));
+  });
+});
